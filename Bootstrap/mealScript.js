da04c391ab48a3555861ad31389afd028fd10859
Trey <79815932+MstrMnd9@users.noreply.github.com>
2025-04-30 16:44:24 -0500

    Update mealScript.js

--- Bootstrap/mealScript.js
+++ Bootstrap/mealScript.js
@@ -1,8 +1,17 @@
 let mealList = [];
 let priceList = [];
 let timeList = [];
+var table = document.getElementById("meal-table")
 function addMeal () {
-  mealList.push(document.getElementById("meal-name").value);
-  priceList.push(document.getElementById("meal-price").value);
-  timeList.push(document.getElementById("meal-type").value);
+  mealList.shift(document.getElementById("meal-name").value);
+  priceList.shift(document.getElementById("meal-price").value);
+  timeList.shift(document.getElementById("meal-type").value);
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  cell1.innerHTML = "mealList[0]";
+  cell2.innerHTML = "priceList[0]";
+  cell3.innerHTML = "timeList[0];
+  
 }
